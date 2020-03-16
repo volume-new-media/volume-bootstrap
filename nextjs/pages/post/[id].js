2f22f1ff32112fded8f8ddf90c76a2376eb0c9a6
Taz <495686-tazran@users.noreply.gitlab.com>
2020-03-16 21:28:35 +1100

    reverted back to plain js, updated api uri

--- nextjs/pages/post/[id].js
+++ nextjs/pages/post/[id].js
@@ -1,6 +1,5 @@
 import { useRouter } from 'next/router'
 import fetch from 'node-fetch'
-import { GetStaticProps } from 'next'
 
 const Post = ({ post }) => {
     const router = useRouter()
@@ -12,13 +11,12 @@ const Post = ({ post }) => {
     )
 }
 
-export const getStaticProps: GetStaticProps = async context => {
+export async function getStaticProps(context) {
     // /post/{id}
     const id = context.params.id
-    // const id: Number = 1
 
     // Call an external API endpoint to get posts.
-    const res = await fetch(`https://headless-backend.lndo.site/wp-json/acf/v3/posts/${id}`)
+    const res = await fetch(`https://headless-wordpress.lndo.site/wp-json/wp/v2/posts/${id}`)
     const post = await res.json()
 
     // By returning { props: posts }, the Blog component
@@ -30,23 +28,11 @@ export const getStaticProps: GetStaticProps = async context => {
     }
 }
 
-// export const getStaticPaths: GetStaticPaths = async () => {
-//   return {
-//     paths: [
-//       { params: { id: 1 } },
-//     ],
-//     fallback: true,
-//   }
-// }
 export async function getStaticPaths() {
     return {
         paths: [{ params: { id: '1' } }],
-        fallback: true // See the "fallback" section below
+        fallback: true // Load posts from the server
     };
 }
-
-// export const getServerSideProps: GetServerSideProps = async context => {
-//     return {}
-// }
 
 export default Post
