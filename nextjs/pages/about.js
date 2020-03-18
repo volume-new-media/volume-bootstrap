068e6488d127e2e663af7ec022f0d1dfd13e1f95
Taran <taran@volume.net.au>
2020-03-18 16:10:38 +1100

    add about, contct

---
+++ nextjs/pages/about.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Layout from '../components/layout'
+
+const About = () => {
+  return (
+    <Layout title="About">
+      <h1>About</h1>
+    </Layout>
+  )
+}
+
+export default About
---
+++ nextjs/pages/contact.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Layout from '../components/layout'
+
+const Contact = () => {
+  return (
+    <Layout title="Contact">
+      <h1>Contact</h1>
+    </Layout>
+  )
+}
+
+export default Contact
